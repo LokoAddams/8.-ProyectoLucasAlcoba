da31ebd7fe0bddcee64be76f63df309f5c0065ee
LokoAddams <lucasalcoba74@gmail.com>
2026-03-08 15:12:39 -0400

    first mobile

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const h1 = document.querySelector("header h1");
+const headerHiddenClass = "hidden";
+const scrollThreshold = 50;
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    h1.classList.add("hidden");
+  if (window.scrollY > scrollThreshold) {
+    h1.classList.add(headerHiddenClass);
   } else {
-    h1.classList.remove("hidden");
+    h1.classList.remove(headerHiddenClass);
   }
 });
